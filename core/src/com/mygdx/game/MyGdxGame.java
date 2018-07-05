441d022d650b6709aee740b94fd74da9b1ad9385
missionbit <21johern>
2018-07-05 10:17:14 -0700

    Added States

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,30 +5,36 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.states.GameStateManager;
+import com.mygdx.game.states.MenuState;
 
 public class MyGdxGame extends ApplicationAdapter {
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 480;
+	public static final String TITLE = "Flappy Bird";
+	private GameStateManager gsm;
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
-		System.out.println("Hello World!");
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+		gsm.push(new MenuState(gsm));
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 	}
+
+
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
